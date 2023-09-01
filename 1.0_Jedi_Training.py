a2ea03f91d00bce7dd1ac047b56b011ff751fbb5
choudhuryama <choudhuryama@icloud.com>
2023-09-01 08:59:24 -0500

    My 1st commit

--- 1.0_Jedi_Training.py
+++ 1.0_Jedi_Training.py
@@ -1,21 +1,21 @@
 '''
-1.0 Jedi Training (17pts)  Name:________________
+1.0 Jedi Training (17pts)  Name:Amal
 
 
 1. Define Forking (1pt): 
-
+A fork is a new repository that shares code with the original “upstream” repository.
 2. Define Cloning (1pt): 
-
+making an exact copy of a program, or anything
 3. Define Branching (1pt):
-
+creating copies of programs or objects in development to work in parallel versions, keep the original code and make chnages, and experiment on the branch
 4. Define Committing (1pt): 
-
+an operation which sends the latest changes of the source code to the repository
 5. Define Merging (1pt): 
-
+merges 2 programs and keeps what works, and throws out what dosent
 6. Define Pushing (1pt):
-
+the act of sending, or uploading, code from a branch in your local source code repository to a branch in the remote repository
 7. Define Pull Request (1pt):
-
+ initiates the process of integrating new code changes into the main project repository
 
 8. TURTORIAL ART (10pts.)
 
@@ -34,20 +34,25 @@ yoda.speed(10)  # speed of drawing. Go fast to not waste time.
 yoda.color("#00FF00")
 yoda.circle(100)  #head
 yoda.penup()
-yoda.setpos(50,185) #right ear
+yoda.setpos(50,90) #right eye
 yoda.pendown()
-yoda.goto(200,210)
+yoda.circle(20)
+yoda.penup()
 yoda.goto(88,145)
 yoda.penup()
-yoda.setpos(-50,185)  #left ear
+yoda.setpos(-50,90)  #left eye
 yoda.pendown()
-yoda.goto(-200,210)
-yoda.goto(-88,145)
+yoda.circle(20)
+yoda.penup()
+
+yoda.goto(-50,50)
+yoda.pendown()
+yoda.goto(50,50)
 yoda.penup()
 yoda.setpos(200,-300)
 yoda.pendown()
 yoda.pencolor('#00FF00')
 
 
-yoda.write('Sign Your Name Here',font=("Arial", 16, "normal")) # signs your name to your art
+yoda.write('Amal',font=("Arial", 16, "normal")) # signs your name to your art
 turtle.exitonclick() #Keeps pycharm window open so we can see the drawing
